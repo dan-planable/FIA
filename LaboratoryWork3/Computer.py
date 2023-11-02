f825fde968d55040a88c13b4aa78dd650f299242
dan-planable <danpogorevici@planable.io>
2023-11-02 08:14:10 +0200

    👌 Add third lab

--- LaboratoryWork3/Computer.py
+++ LaboratoryWork3/Computer.py
@@ -8,6 +8,107 @@ import random
 
 logger = Logger()
 
+piece_value = {
+    'pawn': 1,
+    'knight': 3,
+    'bishop': 3,
+    'rook': 5,
+    'queen': 9,
+    'king': 0  
+}
+
+positional_value = {
+    'pawn': [[0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 1, 1, 1, 1, 1, 1, 1],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]],
+    'knight': [[0, 3, 0, 0, 0, 0, 3, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]],
+    'bishop': [[0, 0, 3, 0, 0, 3, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]],
+    'rook': [[5, 0, 0, 0, 0, 0, 0, 5],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]],
+    'queen': [[0, 0, 0, 9, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]],
+    'king': [[0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]]
+}
+
+def calculate_material_value(board, color):
+    material_value = 0
+    for i in range(8):
+        for j in range(8):
+            piece = board[i][j]
+            if isinstance(piece, ChessPiece) and piece.color == color:
+                material_value += piece_value[piece.piece_type]
+    return material_value
+
+def calculate_positional_value(board, color):
+    positional_value = 0
+    for i in range(8):
+        for j in range(8):
+            piece = board[i][j]
+            if isinstance(piece, ChessPiece) and piece.color == color:
+                positional_value += positional_value[piece.piece_type][i][j]
+    return positional_value
+
+def evaluate(board):
+    player_color = board.get_player_color()
+    opponent_color = board.get_opponent_color()
+
+    material_value = calculate_material_value(board, player_color) - calculate_material_value(board, opponent_color)
+    
+    positional_value = calculate_positional_value(board, player_color) - calculate_positional_value(board, opponent_color)
+    
+    piece_activity_value = calculate_piece_activity(board, player_color) - calculate_piece_activity(board, opponent_color)
+
+    final_score = material_value + positional_value + piece_activity_value
+
+    return final_score
+
+def calculate_piece_activity(board, color):
+    piece_activity_value = 0
+    for i in range(8):
+        for j in range(8):
+            piece = board[i][j]
+            if isinstance(piece, ChessPiece) and piece.color == color:
+                moves = piece.filter_moves(piece.get_moves(board), board)
+                piece_activity_value += len(moves)
+    return piece_activity_value
 
 def log_tree(func):
     @wraps(func)
@@ -19,6 +120,20 @@ def log_tree(func):
         return func(*args, **kwargs)
     return wrapper
 
+def get_ai_move(board):
+    moves = minimax(board, board.depth, -math.inf, math.inf, True, True, [[], 0])
+    if board.log:
+        logger.write()
+    if len(moves[0]) == 0:
+        return False
+    best_score = max(moves[0], key=lambda x: x[2])[2]
+    piece_and_move = random.choice([move for move in moves[0] if move[2] == best_score])
+    piece = piece_and_move[0]
+    move = piece_and_move[1]
+    if isinstance(piece, ChessPiece) and len(move) > 0:
+        board.make_move(piece, move[0], move[1])
+    return True
+
 
 def write_to_file(board: Board, current_depth):
     global logger
@@ -28,14 +143,65 @@ def write_to_file(board: Board, current_depth):
     logger.append(board_repr)
 
 
+@log_tree
+def minimax(board, depth, alpha, beta, max_player, save_move, data):
+
+    if depth == 0 or board.is_terminal():
+        data[1] = board.evaluate()
+        return data
+
+    if max_player:
+        max_eval = -math.inf
+        for i in range(8):
+            for j in range(8):
+                if isinstance(board[i][j], ChessPiece) and board[i][j].color != board.get_player_color():
+                    piece = board[i][j]
+                    moves = piece.filter_moves(piece.get_moves(board), board)
+                    for move in moves:
+                        board.make_move(piece, move[0], move[1], keep_history=True)
+                        evaluation = minimax(board, depth - 1, alpha, beta, False, False, data)[1]
+                        if save_move:
+                            if evaluation >= max_eval:
+                                if evaluation > data[1]:
+                                    data.clear()
+                                    data[1] = evaluation
+                                    data[0] = [piece, move, evaluation]
+                                elif evaluation == data[1]:
+                                    data[0].append([piece, move, evaluation])
+                        board.unmake_move(piece)
+                        max_eval = max(max_eval, evaluation)
+                        alpha = max(alpha, evaluation)
+                        if beta <= alpha:
+                            break
+        return data
+    else:
+        min_eval = math.inf
+        for i in range(8):
+            for j in range(8):
+                if isinstance(board[i][j], ChessPiece) and board[i][j].color == board.get_player_color():
+                    piece = board[i][j]
+                    moves = piece.get_moves(board)
+                    for move in moves:
+                        board.make_move(piece, move[0], move[1], keep_history=True)
+                        evaluation = minimax(board, depth - 1, alpha, beta, True, False, data)[1]
+                        board.unmake_move(piece)
+                        min_eval = min(min_eval, evaluation)
+                        beta = min(beta, evaluation)
+                        if beta <= alpha:
+                            break
+        return data
+
 
 def get_ai_move(board):
-    # moves = minimax(board, board.depth, -math.inf, math.inf, True, True, [[], 0])
-    # if board.log:
-    #     logger.write()
-
-    piece, move = get_random_move(board)
-
+    moves = minimax(board, board.depth, -math.inf, math.inf, True, True, [[], 0])
+    if board.log:
+        logger.write()
+    if len(moves[0]) == 0:
+        return False
+    best_score = max(moves[0], key=lambda x: x[2])[2]
+    piece_and_move = random.choice([move for move in moves[0] if move[2] == best_score])
+    piece = piece_and_move[0]
+    move = piece_and_move[1]
     if isinstance(piece, ChessPiece) and len(move) > 0 and isinstance(move, tuple):
         board.make_move(piece, move[0], move[1])
     return True
@@ -58,8 +224,5 @@ def get_random_move(board):
         return
     piece = random.choice(pieces)
     move = random.choice(moves[pieces.index(piece)])
-    
-    return piece, move
-    # print("move:", move)
-    # if isinstance(piece, ChessPiece) and len(move) > 0:
-    #     board.make_move(piece, move[0], move[1])
+    if isinstance(piece, ChessPiece) and len(move) > 0:
+        board.make_move(piece, move[0], move[1])
